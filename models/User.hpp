27c31eebb92bd27e13edae52157ea8ed65d07014
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-01-21 14:21:04 +0000

    Add models Task and User

---
+++ models/User.hpp
@@ -0,0 +1,78 @@
+#ifndef __User_H__
+#define __User_H__
+
+#include "BaseEntity.hpp"
+
+#include <string>
+
+using namespace std;
+
+class User : public BaseEntity
+{
+
+private:
+    string firstname;
+    string lastname;
+    string email;
+    string password;
+
+public:
+    User(string firstname, string lastname, string email, string password)
+    {
+        this->id = ++User::id;
+        this->firstname = firstname;
+        this->lastname = lastname;
+        this->email = email;
+        this->password = password;
+        BaseEntity();
+    };
+
+    // getters
+    string getFirstname()
+    {
+        return this->firstname;
+    }
+
+    string getLastname()
+    {
+        return this->lastname;
+    }
+
+    int getTaskId()
+    {
+        return this->taskId;
+    }
+
+    string getEmail()
+    {
+        return this->email;
+    }
+
+    string getPassword()
+    {
+        return this->password;
+    }
+
+    // setters
+    void setFirstname(string firstname)
+    {
+        this->firstname = firstname;
+    }
+
+    void setLastname(string lastname)
+    {
+        this->lastname = lastname;
+    }
+
+    void setEmail(string email)
+    {
+        this->email = email;
+    }
+
+    void setPassword(string password)
+    {
+        this->password = password;
+    }
+};
+
+#endif //__User_H__
