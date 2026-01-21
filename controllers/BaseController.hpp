94ca39042d4845e4ae71772c90322cb701278456
nosleepman1 <abdallahdiouf.dev@gmail.com>
2026-01-21 16:20:28 +0000

    init controllers Base and User

---
+++ controllers/BaseController.hpp
@@ -0,0 +1,16 @@
+#ifndef __BASECONTROLLER_H__
+#define __BASECONTROLLER_H__
+
+#include <string>
+
+using namespace std;
+
+class BaseContoller
+{
+private:
+public:
+    BaseContoller() {};
+    ~BaseContoller() {}
+};
+
+#endif //__BASECONTROLLER_H__
